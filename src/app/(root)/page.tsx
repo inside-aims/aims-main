572f41e27e0ecfe090f5feb727ba2c4628daeddf
s-kvng <kvngnathan8420@gmail.com>
2025-01-02 20:30:45 +0000

    refactor nav & hero ui

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,11 @@
-import { Button } from "@nextui-org/button";
 
 import HeroSection from "@/components/hocs/Home/HeroSection";
-import Navbar from "@/components/shared/navigation/navbar";
 
 export default function Home() {
   return (
     <main className="">
-      <Navbar />
       <HeroSection />
+      
     </main>
   );
 }
